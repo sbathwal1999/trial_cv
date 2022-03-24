b0cc089edf1415dd565089e59da8fdc65762e31e
Shubham Bathwal <sbathwal1999@gmail.com>
2022-03-24 16:11:44 +0530

    trial

---
+++ basic.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5,
+                          min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        img = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run(debug=True)
